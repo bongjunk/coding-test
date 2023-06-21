779bbe255d7a5b2405e5e4861cbb601f56c2a716
BongjunKim <74786253+bongjunk@users.noreply.github.com>
2023-06-21 18:06:48 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,11 @@
+const solution = (s) => {
+  let answer = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      answer.push(s[i]);
+    }
+  }
+
+  return answer.sort().join("");
+};
