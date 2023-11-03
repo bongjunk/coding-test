3b51c7686fa504d8952131d6c86e9e6aa2963c61
BongjunKim <74786253+bongjunk@users.noreply.github.com>
2023-11-03 17:17:27 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,12 +1,8 @@
-const solution = (n, words) => {
-  let turn = 1;
+function solution(n, words) {
+    var answer = [];
 
-  for (let i = 1; i < words.length; i++) {
-    const cur = (i % n) + 1;
-    if (words[i - 1].slice(-1) !== words[i][0]) return [cur, turn];
-    if (words.findIndex((el) => el === words[i]) !== i)
-      return [cur, turn];
-    if (cur === n) turn += 1;
-  }
-  return [0, 0]
-};
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+    console.log('Hello Javascript')
+
+    return answer;
+}
