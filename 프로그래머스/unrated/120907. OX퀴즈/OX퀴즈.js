6ff74e049da8d73538a11e8fb1f72c5d431e66bc
BongjunKim <74786253+bongjunk@users.noreply.github.com>
2023-11-30 11:09:55 +0900

    [level unrated] Title: OX퀴즈, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.js
@@ -2,7 +2,6 @@ const solution = (quiz) => {
   let answer = [];
   for (let i = 0; i < quiz.length; i++) {
     let [left, right] = quiz[i].split("=");
-    let leftCer = left.split(" ").join("");
     eval(left) === Number(right) ? answer.push("O") : answer.push("X");
   }
   return answer;
